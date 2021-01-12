8369ac21326621761d8f91ed0cbbaa28bfd1a953
francisudeji <francis.udejiofficial@gmail.com>
2021-01-12 10:05:26 +0100

    Add eslint and prettier config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import '../css/tailwind.css'
+import * as React from 'react'
+import Head from 'next/head'
+
+interface FunctionComponent {
+  Component: React.FunctionComponent
+  pageProps: Record<string, unknown>
+}
+
+const App: React.FunctionComponent<FunctionComponent> = ({ Component, pageProps }) => {
+  return (
+    <div className="antialiased text-gray-900 min-h-screen">
+      <Head>
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#FFE81F" />
+        <title>The Shoppies: Movie awards for entrepreneurs</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="h-full min-h-screen py-32 bg-black flex items-center justify-center">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-red-500">hello world</h1>
+    </div>
+  )
+}
